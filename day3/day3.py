5a54009e3026a81b6d3750f68ec39c09772f43a3
Stefano Prosperi <step.prosperi@gmail.com>
2019-12-09 16:17:17 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -8,8 +8,8 @@ dy = {'<': 0, '>': 0, '^': 1, 'v': -1}
 s = [set(), set()]
 s[0].add((x[0], y[0]))
 s[1].add((x[1], y[1]))
-for idx, c in enumerate(input):
-    x[idx % 2] += dx[c]
-    y[idx % 2] += dy[c]
-    s[idx % 2].add((x[idx % 2], y[idx % 2]))
+for i, c in enumerate(input):
+    x[i % 2] += dx[c]
+    y[i % 2] += dy[c]
+    s[i % 2].add((x[i % 2], y[i % 2]))
 print(len(s[0] | s[1]))
