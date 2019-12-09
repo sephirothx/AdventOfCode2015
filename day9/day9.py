4421f330f203ad0ece707a62787280c441c7a564
Stefano Prosperi <step.prosperi@gmail.com>
2019-12-09 21:22:00 +0100

    day9

---
+++ day9/day9.py
@@ -0,0 +1,22 @@
+import networkx.algorithms
+from itertools import permutations
+
+with open("./input.txt") as f:
+    INPUT = f.read().split('\n')
+
+g = networkx.Graph()
+for s in INPUT:
+    a = s.split(' ')
+    g.add_edge(a[0], a[2], dist=int(a[4]))
+
+ans1, ans2 = 10000, 0
+for p in permutations(g.nodes()):
+    d = 0
+    for i, n in enumerate(p):
+        if i == 0:
+            continue
+        d += g[n][p[i-1]]["dist"]
+    ans1 = min(ans1, d)
+    ans2 = max(ans2, d)
+
+print(ans1, ans2)
